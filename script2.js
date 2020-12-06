5b5cf98319c4c2862d0dabf1d7a6d697310a4e9e
andrea delgado <eugenia_del87@hotmail.com>
2020-12-06 15:18:31 -0300

    other other changes

--- script2.js
+++ script2.js
@@ -6,115 +6,75 @@ gsap.to('.scroll', {
     stagger: 1,
     scrollTrigger:{
         /*scroller: ".containerScroll",*/
-        trigger: '.wrapper',
+        trigger: '.scroll',
         start: '30% center',
         end: '30% center',
-        /*markers:true,*/    
+        /*markers:true,*/   
         scrub: 1,
         opacity: 0,
         toggleActions: 'restart pause reverse reset'
     }
 })
 
- /*
-gsap.from('.hijo', {
-    duration:3,
-    y: "-20vh",
-    ease: "none",
-    opacity: 0,
-    stagger: 1,
-    scrollTrigger:{
-        /*scroller: ".containerScroll",
-        trigger: '.wrapper',
-        start: '90% center',
-        end: '90% center',
-        markers:true, 
-        opacity: 1,   
-        scrub: 1,
-        toggleActions: 'restart pause reverse reset'
-    }
-})
-
-gsap.to('.hijo', {
-    duration:3,
-    y: "20vh",
-    ease: "none",
-    opacity: 1,
-    stagger: 6,
-    scrollTrigger:{
-        /*scroller: ".containerScroll",
-        trigger: '.wrapper',
-        start: '70% center',
-        end: '90% center',
-        markers:true,    
-        scrub: 1,
-        toggleActions: 'restart pause reverse reset'
-    }
-})*/
-
-gsap.from('.anim1', {opacity:0, duration:1, y:-50, ease: 'Power2.easeInOut', stagger:0.1,  
+gsap.from('.anim1', {opacity:0, duration:2, y:-70, ease: 'Power2.easeInOut', stagger:0.1,  
 scrollTrigger:{
     trigger: '.anim1',
-    start: '-20% center',
-    end: '15% center',
+    start: '-50% center',
+    end: '5% center',
     markers:true,    
-    scrub: 1,
+    scrub: 2,
     toggleActions: 'restart pause reverse reset'
  }
 })
 
-gsap.from('.anim2', {opacity:0, duration:2, y:-50, ease: 'Power2.easeInOut', stagger:0.1,  
+gsap.from('.anim2', {opacity:0, duration:2, y:-70, ease: 'Power2.easeInOut', stagger:0.1,  
 scrollTrigger:{
     trigger: '.anim2',
-    start: '-20% center',
-    end: '15% center',
+    start: '-50% center',
+    end: '-5% center',
     markers:true,    
-    scrub: 1,
+    scrub: 2,
     toggleActions: 'restart pause reverse reset'
  }
 })
 
-gsap.from('.anim3', {opacity:0, duration:2, y:-50, ease: 'Power2.easeInOut', stagger:0.1,  
+gsap.from('.anim3', {opacity:0, duration:2, y:-70, ease: 'Power2.easeInOut', stagger:0.1,  
 scrollTrigger:{
     trigger: '.anim3',
-    start: '-20% center',
-    end: '15% center',
+    start: '-50% center',
+    end: '-5% center',
     markers:true,    
-    scrub: 1,
+    scrub: 2,
     toggleActions: 'restart pause reverse reset'
  }
 })
 
-gsap.from('.anim4', {opacity:0, duration:2, y:-50, ease: 'Power2.easeInOut', stagger:0.1,  
+gsap.from('.anim4', {opacity:0, duration:2, y:-70, ease: 'Power2.easeInOut', stagger:0.1,  
 scrollTrigger:{
     trigger: '.anim4',
-    start: '-20% center',
-    end: '15% center',
+    start: '-50% center',
+    end: '-5% center',
     markers:true,    
-    scrub: 1,
+    scrub: 2,
     toggleActions: 'restart pause reverse reset'
  }
 })
 
-gsap.from('.anim5', {opacity:0, duration:2, y:-50, ease: 'Power2.easeInOut', stagger:0.1,  
+gsap.from('.anim5', {opacity:0, duration:2, y:-70, ease: 'Power2.easeInOut', stagger:0.1,  
 scrollTrigger:{
     trigger: '.anim5',
-    start: '-20% center',
-    end: '15% center',
+    start: '-50% center',
+    end: '-5% center',
     markers:true,    
-    scrub: 1,
+    scrub: 4,
     toggleActions: 'restart pause reverse reset'
  }
 })
+
+
 
 var tl = gsap.timeline({defaults:{duration:1}})
 
 tl.from(".fill, .fill2", {y:-20, stagger: .6})
   .to(rule, {duration:1, cssRule: {scaleY:0}}, "-=2.2")
-  
-
-
-
-
-
-  
+   
